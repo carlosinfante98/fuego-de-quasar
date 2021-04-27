cec8072916c78b1abf9f6986bd12610ef43225f9
Carlos Infante <carlos98infante@gmail.com>
2021-04-27 13:33:51 -0500

    Arreglar servidor

--- models/server.js
+++ models/server.js
@@ -1,15 +1,35 @@
+// Modulos para crear el servidor
 const express = require('express');
+const routes = require('../routes/routes');
 
 class Server {
     constructor() {
+        // Configuracion del servidor
         this.app = express();
         this.port = 3001;
+
+        // Middlewares
+        this.middlewares();
+
+        // Rutas de mi aplicacion
+        this.generalRoutes();
+    }
+
+    middlewares() {
+
+        // Parseo y lectura del body
+        this.app.use(express.json());
+    }
+
+    generalRoutes() {
+        this.app.use('/', routes);
     }
 
     listen() {
         this.app.listen(this.port, () => {
             console.log('Servidor corriendo en puerto', this.port);
         });
+
     }
 }
 
