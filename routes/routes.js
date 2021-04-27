de02d858f386e8afed9ef11a561a34f09c77d208
Carlos Infante <carlos98infante@gmail.com>
2021-04-27 07:54:23 -0500

    Rutas agregadas

---
+++ routes/routes.js
@@ -0,0 +1,26 @@
+const { Router } = require("express");
+const { check } = require('express-validator');
+const { validateFields } = require("../middlewares/validate-fields");
+const { setSecrets, setSecret, getFinalSecret } = require("../controllers/ship-controller");
+
+const router = Router();
+
+// Metodo POST para todos los mensajes y distancias de los satelites
+router.post("/topsecret", [
+    check('satellites', 'No es un arreglo de satelites valido').isArray(),
+    check('satellites.*.name', 'Los satelites no pueden tener nombres vacios').not().isEmpty(),
+    validateFields
+], setSecrets);
+
+// Metodo POST para mensaje y distancia de un satelite
+router.post("/topsecret_split/:satellite_name", [
+    check('satellite_name', 'No es un nombre valido').not().isEmpty(),
+    validateFields
+], setSecret);
+
+// Metodo GET para el mensaje y la posicion de la nave
+router.get("/topsecret_split", [
+    validateFields
+], getFinalSecret);
+
+module.exports = router;
