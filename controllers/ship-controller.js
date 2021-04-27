b945c6171071cc44698e0b5734349356f9fb8b09
Carlos Infante <carlos98infante@gmail.com>
2021-04-27 09:34:34 -0500

    Cambio en los metodos de endpoints

--- controllers/ship-controller.js
+++ controllers/ship-controller.js
@@ -1,20 +1,20 @@
 const { getLocation, getMessage, updateSatellites } = require('../services/ship-services');
 
 
-const postSatellitesMessages = (req, res, next) => {
+const postSatellitesMessages = (req, res) => {
     try {
         let satellites = req.body.satellites;
         updateSatellites(satellites);
         const position = getLocation();
         const message = getMessage();
         res.json({ position, message });
+        return res;
     } catch (err) {
         res.status(404).json('No hay información suficiente para obtener mensaje o posición de la nave');
     }
-    next();
 }
 
-const postSingleMessage = (req, res, next) => {
+const postSingleMessage = (req, res) => {
     try {
         const name = req.params.satellite_name;
         const distance = req.body.distance;
@@ -28,21 +28,21 @@ const postSingleMessage = (req, res, next) => {
         const position = getLocation();
         const msg = getMessage();
         res.json({ position, msg });
+        return res;
     } catch (err) {
         res.status(404).json('No hay información suficiente para obtener mensaje o posición de la nave');
     }
-    next();
 }
 
-const getFinalSecret = (req, res, next) => {
+const getFinalSecret = (req, res) => {
     try {
         const position = getLocation();
         const message = getMessage();
         res.json({ position, message });
+        return res;
     } catch (err) {
         res.status(400).json('No hay información suficiente para obtener mensaje o posición de la nave');
     }
-    next();
 }
 
 module.exports = {
